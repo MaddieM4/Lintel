80ada1ec08e24066977f5c64098b56644248d407
Philip Horger <campadrenalin@gmail.com>
2015-06-21 17:22:30 -0700

    Break out Lintel::FutureBuilder base class

--- Lintel/Template/Builder.pm
+++ Lintel/Template/Builder.pm
@@ -1,6 +1,7 @@
 package Lintel::Template::Builder;
 use Moose;
-use Future;
+
+extends 'Lintel::FutureBuilder';
 
 has 'tt' => (
 	is  => 'rw',
@@ -13,59 +14,6 @@ has 'name' => (
 	isa => 'Str',
 	required => 1,
 );
-
-has 'args' => (
-	is  => 'rw',
-	isa => 'HashRef',
-	required => 1,
-);
-
-has '_required' => (
-	is  => 'ro',
-	isa => 'ArrayRef[Future]',
-	default => sub { [] },
-);
-has '_optional' => (
-	is  => 'ro',
-	isa => 'ArrayRef[Future]',
-	default => sub { [] },
-);
-
-sub _prep_future {
-	my ($self, $param, $future) = @_;
-	die "Not a Future" if !$future->isa('Future');
-
-	if (ref $param eq 'CODE') {
-		$future->on_done(sub {
-			return $param->($self, @_)
-		});
-	} else {
-		$future->on_done(sub {
-			$self->args->{$param} = shift;
-		});
-	}
-	return $future;
-}
-
-sub req {
-	my ($self, $param, $future) = @_;
-	push(@{$self->_required}, $self->_prep_future($param, $future));
-	return $self;
-}
-
-sub opt {
-	my ($self, $param, $future) = @_;
-	push(@{$self->_optional}, $self->_prep_future($param, $future));
-	return $self;
-}
-
-sub collect {
-	my $self = shift;
-	return Future->needs_all(
-		Future->needs_all(@{$self->_required}),
-		Future->wait_all(@{$self->_optional}),
-	);
-}
 
 sub execute {
 	my $self = shift;
