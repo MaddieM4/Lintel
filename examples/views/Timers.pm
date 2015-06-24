5e7af66f604840ee7227cd9f0fa0b76f06332e62
Philip Horger <campadrenalin@gmail.com>
2015-06-24 09:59:00 -0700

    Add form interface for adjusting concurrency on Timers demo

--- examples/views/Timers.pm
+++ examples/views/Timers.pm
@@ -16,12 +16,20 @@ sub timers {
 	local $AnyEvent::HTTP::MAX_PER_HOST = $req->parameters->{concurrency}
 		if $req->parameters->{concurrency};
 
+	my $builder = $tt->build('timers.tmpl',
+		title => 'Timer Demo',
+		timers => [],
+		concurrency => $AnyEvent::HTTP::MAX_PER_HOST,
+	);
+
+	# Add a potentially variable number of dependencies.
+	# Use a callback sub instead of a param name, to accumulate in 'timers'.
 	my $callback = sub {
 		my ($builder, $result) = @_;
 		push(@{$builder->args->{timers}}, $result);
 	};
-	my $builder = $tt->build('timers.tmpl', title => 'Timer Demo', timers => []);
 	map { $builder->req($callback, $api->get('/api/wait/1s')) } (0..19);
+
 	return $builder->execute;
 }
 
